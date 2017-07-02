4893709bc27b3b58920a2c6c94c09b23ead7f36b
Hugo Carvalho <warderld@hotmail.com>
2017-07-02 18:55:14 +0100

    Alpha deploy
    First website test version going live.

---
+++ intro.js
@@ -0,0 +1,10 @@
+$("#lf").click(
+    function() {
+    $(this).slideUp();
+    lp.classList.add("fadeout");
+    wrapper.classList.add("white");
+    lp.classList.add("white");
+    main.classList.add("mainFadein");
+    murloc.classList.add("fadein");    
+    }
+  );
