492c277949bccb7a73daa7185f04bb21d0bdfd04
Hugo Carvalho <warderld@hotmail.com>
2017-07-02 21:43:03 +0100

    jQ test3

--- intro.js
+++ intro.js
@@ -1,12 +1,8 @@
-$(document).ready(	function(){
-	$('#lf').on( 'click', 
-    		function() {
+document.getElementById('lf').addEventListener( 'click', function(){
     			$(this).slideUp();
     			lp.classList.add('fadeout');
     			wrapper.classList.add('white');
     			lp.classList.add('white');
     			main.classList.add('mainFadein');
     			murloc.classList.add('fadein');    
-    		}
-  	);
-});
+    		});
