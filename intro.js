c3962cd3e5ce997857dfc45e369c148d1da59546
murlokito <murlux@protonmail.com>
2019-05-08 23:34:06 +0100

    removing js and refactoring css

--- intro.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-$(".landing").one( 'click', function(){
- $(this).slideUp();
- document.getElementById('lp').classList.add('fadeout', 'white');
- document.getElementById('wrapper').classList.add('white');
- document.getElementById('main').classList.add('mainFadein');
- document.getElementById('murloc').classList.add('fadein');    
-});
