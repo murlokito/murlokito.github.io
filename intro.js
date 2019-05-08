f694b470d158c0d608ea1a1090763f7e87bf944a
murlokito <murlux@protonmail.com>
2019-05-08 23:08:47 +0100

    Life update.

--- intro.js
+++ intro.js
@@ -1,7 +1,8 @@
+'use strict'
 $(".landing").one( 'click', function(){
-    			$(this).slideUp();
-    			document.getElementById('lp').classList.add('fadeout', 'white');
-    			document.getElementById('wrapper').classList.add('white');
-    			document.getElementById('main').classList.add('mainFadein');
-    			document.getElementById('murloc').classList.add('fadein');    
-    		});
+ $(this).slideUp();
+ document.getElementById('lp').classList.add('fadeout', 'white');
+ document.getElementById('wrapper').classList.add('white');
+ document.getElementById('main').classList.add('mainFadein');
+ document.getElementById('murloc').classList.add('fadein');    
+});
